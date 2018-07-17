6d6d1aeba8c926bae89a52a6200c88801623e896
Lucas Pedro <lucas.pedro@enext.local>
2018-07-17 20:31:54 -0300

    Modificado formato de conexão no arquivo de conexão com banco na pasta de exemplos

--- exemplos/conexaoBD/pdo.class.php
+++ exemplos/conexaoBD/pdo.class.php
@@ -1,59 +1,27 @@
 <?php
-/**
- * Classe de conexão ao banco de dados usando PDO no padrão Singleton.
- * Modo de Usar:
- * require_once './Database.class.php';
- * $db = Database::conexao();
- * E agora use as funções do PDO (prepare, query, exec) em cima da variável $db.
- */
-class Database
-{
-    # Variável que guarda a conexão PDO.
-    protected static $db;
 
-    # Private construct - garante que a classe só possa ser instanciada internamente.
-    private function __construct()
+class conexao{
+    //Define as variaveis
+    public $host;
+    public $user;
+    public $pass;
+
+    public function __construct()
     {
-        # Informações sobre o banco de dados:
-        $db_host = "mysql.w2it.com.br";
-        $db_nome = "etec_teste_1";
-        $db_usuario = "etecdev";
-        $db_senha = "5J%2dt2ds";
-        $db_driver = "mysql";
-
-        # Informações sobre o sistema:
-        $sistema_titulo = "Projteo Cidadão";
-        $sistema_email = "gabriel.gutierrezhp@gmail.com";
-
-        try
-        {
-            # Atribui o objeto PDO à variável $db.
-            self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_nome", $db_usuario, $db_senha);
-            # Garante que o PDO lance exceções durante erros.
-            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            # Garante que os dados sejam armazenados com codificação UFT-8.
-            self::$db->exec('SET NAMES utf8');
+        //Atribui valor as variaveis
+        $this->host = "mysql:host=mysql.w2it.com.br;dbname=etec_teste_1";
+        $this->user = "etecdev";
+        $this->pass = "5J%2dt2ds";
+    }
+    public function conect()
+    {
+        //tenta conexão com banco retorna conexão caso tenha sucesso, caso não, retorna mensagem de erro.
+        try{
+            $con = new PDO($this->host, $this->user, $this->pass);
+            return $con;
         }
-        catch (PDOException $e)
-        {
-            # Envia um e-mail para o e-mail oficial do sistema, em caso de erro de conexão.
-            mail($sistema_email, "PDOException em $sistema_titulo", $e->getMessage());
-            # Então não carrega nada mais da página.
-            die("Connection Error: " . $e->getMessage());
+        catch (PDOException $ex){
+            return "Erro ao conectar, Erro: ".$ex->getMessage();
         }
     }
-
-    # Método estático - acessível sem instanciação.
-    public static function conexao()
-    {
-        # Garante uma única instância. Se não existe uma conexão, criamos uma nova.
-        if (!self::$db)
-        {
-            new Database();
-        }
-
-        # Retorna a conexão.
-        return self::$db;
-    }
-
 }
