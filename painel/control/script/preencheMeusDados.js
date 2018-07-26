c0e54f3ca31478845e313b8d286e461579123c36
ggpgutierrez <34143293+ggpgutierrez@users.noreply.github.com>
2018-07-26 01:08:52 -0300

    Metodo para Preencher a tela de meus Dados

---
+++ painel/control/script/preencheMeusDados.js
@@ -0,0 +1,25 @@
+$.get( "control/retornaDadosEtec.class.php", function( data ) {
+var response = JSON.parse(data);
+document.getElementById('nomeEtec').value = response[0].nomeEtec; 
+document.getElementById('emailEtec').value = response[0].emailEtec;
+document.getElementById('enderecoEtec').value = response[0].logradouroEtec;
+document.getElementById('cidadeEtec').value = response[0].cidadeEtec;
+document.getElementById('usuarioEtec').value = response[0].nomeUsuario;
+document.getElementById('senhaEtec').value = response[0].senhaEtec;
+document.getElementById('responsavelEtec').value = response[0].nomeResponsavel;
+document.getElementById('diretorEtec').value = response[0].nomeDiretor;
+document.getElementById('telefoneEtec').value = response[0].telefoneEtec;
+document.getElementById('cepEtec').value = response[0].cepEtec;
+document.getElementById('ufEtec').value = response[0].ufEtec;
+
+
+
+
+
+
+
+
+
+
+
+});
