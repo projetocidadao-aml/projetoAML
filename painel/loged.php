7df7acc60e8b47729f15d3f9c755fd9c7ba17358
Alex Santos <alexsantosinformatica@gmail.com>
2018-07-30 17:06:21 -0300

    Estilização, correção de textos

--- painel/loged.php
+++ painel/loged.php
@@ -18,7 +18,7 @@ include 'control/router.class.php';
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark mb-4">
 		<div class="container">
-			<a class="navbar-brand" href="index.php?page=painel">Cidadão do Futuro</a>
+			<a class="navbar-brand" href="index.php?page=painel"><img src="dist/img/logoHeader.png" id="imgHeader"></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
