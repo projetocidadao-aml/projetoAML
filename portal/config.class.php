62a1e956be8a68816dc97080bcdcbd205a53c856
ggpgutierrez <34143293+ggpgutierrez@users.noreply.github.com>
2018-08-12 03:49:20 -0300

    atualização de estrutura de pastas

---
+++ portal/config.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class conexao{
+    //Define as variaveis
+    public $host;
+    public $user;
+    public $pass;
+
+    public function __construct()
+    {
+        //Atribui valor as variaveis
+        $this->host = "mysql:host=mysql.w2it.com.br;dbname=etec_teste_2";
+        $this->user = "etecdev";
+        $this->pass = "5J%2dt2ds";
+    }
+    public function conect()
+    {
+        //tenta conexão com banco retorna conexão caso tenha sucesso, caso não, retorna mensagem de erro.
+        try{
+            $con = new PDO($this->host, $this->user, $this->pass);
+            return $con;
+        }
+        catch (PDOException $ex){
+            return "Erro ao conectar, Erro: ".$ex->getMessage();
+        }
+    }
+}
