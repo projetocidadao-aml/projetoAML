af1ead4a910e4ba9714b95c843bcd14396de6271
Alex Santos <alexsantosinformatica@gmail.com>
2018-08-15 01:09:01 -0300

    Cursos

--- painel/control/capturaCurso.class.php
+++ painel/control/capturaCurso.class.php
@@ -1,9 +1,22 @@
 <?php
-    include 'model/capturaCurso.class.php';
-    class cursoControl
-    {
-        function capturaDados(){
-            $curso = new capturaCursos();
-            $curso->listaCursos();
-        }
-    }
+include 'model/capturaCurso.class.php';
+class cursoControl
+{
+	function capturaDados(){
+		$curso = new capturaCursos();
+		$curso->listaCursos();
+
+	}
+
+	function capturaDadosEtec(){
+		$curso = new capturaCursos();
+		$curso->listaCursosEtec();
+
+	}
+
+		function listaCursosComboBox(){
+		$curso = new capturaCursos();
+		$curso->listaCursosComboBox();
+
+	}
+}
--- painel/model/capturaCurso.class.php
+++ painel/model/capturaCurso.class.php
@@ -1,29 +1,60 @@
 <?php
-    include("config.class.php");
-    class capturaCursos
+include("config.class.php");
+class capturaCursos
+{
+    private $conn;
+    function __construct()
     {
-        private $conn;
-		function __construct()
-		{
-			$conexao = new conexao();
-			$this->conn = $conexao->conect();
-		}
-        function listaCursos(){
-            $sql = "SELECT * FROM CURSOS";
-            $db = $this->conn->prepare($sql);
-            $db->execute();
-			$linha = $db->fetchAll();
-			foreach ($linha as $value) {
-				echo "<tr>
-                        <td>".$value['CURSO_ID']."</td>
-                        <td>".$value['CURSO_NOME']."</td>
-                        <td>".$value['CURSO_DURACAO']."</td>
-                        <td><button class='btn btn-secondary'>Alterar</button></td>
-                    </tr>";
-			}
-            
-        }
+       $conexao = new conexao();
+       $this->conn = $conexao->conect();
+   }
+   function listaCursos(){
+    $sql = "SELECT * FROM CURSOS";
+    $db = $this->conn->prepare($sql);
+    $db->execute();
+    $linha = $db->fetchAll();
+    foreach ($linha as $value) {
+        echo "<tr>
+        <td>".$value['CURSO_ID']."</td>
+        <td>".$value['CURSO_NOME']."</td>
+        <td>".$value['CURSO_DURACAO']."</td>
+        <td><button class='btn btn-secondary'>Alterar</button></td>
+        </tr>";
+    }
+
+}
+
+function listaCursosComboBox(){
+    $sql = "SELECT * FROM CURSOS";
+    $db = $this->conn->prepare($sql);
+    $db->execute();
+    $linha = $db->fetchAll();
+    foreach ($linha as $value) {
+        echo "<option value = '$value[CURSO_ID]'>
+        $value[CURSO_NOME]
+        </option>";
     }
     
+}
+
+
+function listaCursosEtec(){
+
+    $sql = "SELECT * FROM ETEC_CURSOS INNER JOIN CURSOS ON CURSOS.CURSO_ID = ETEC_CURSOS.ID_CURSO WHERE ID_ETEC = $_SESSION[idUser]";
+    $db = $this->conn->prepare($sql);
+    $db->execute();
+    $linha = $db->fetchAll();
+    foreach ($linha as $value) {
+        echo "<tr>
+        <td>".$value['CURSO_ID']."</td>
+        <td>".$value['CURSO_NOME']."</td>
+        <td>".$value['ETEC_CURSO_PERIODO']."</td>
+        <td><button class='btn btn-secondary'>Alterar</button></td>
+        </tr>";
+    }
+
+}
+}
+
 
 ?>
