1cc9ddd12977270b10179f61d0ebe80b46a1a402
ggpgutierrez <34143293+ggpgutierrez@users.noreply.github.com>
2018-08-28 21:47:08 -0300

    Upload de imagem configurado com postagem normal
    Só falta a parte de video

--- portal/model/uploadPost.php
+++ portal/model/uploadPost.php
@@ -1,38 +1,69 @@
 <?php
-session_start();
+ session_start();
+ include("../config.class.php");
+    class postagem{
+        private $conn;
+        private $SendCadImg;
+        private $Grupo;
+        function __construct()
+		{
+            $conexao = new conexao();
+            $this->conn = $conexao->conect();
+            $grupo = $_SESSION['idUser_portal'];
+            $postagem = $_POST['postDesafio'];
+            $perfil_usu = $_SESSION['ID_PERFIL'];
+            $SendCadImg = filter_input(INPUT_POST, 'SendCadImg', FILTER_SANITIZE_STRING);
 
-$SendCadImg = filter_input(INPUT_POST, 'SendCadImg', FILTER_SANITIZE_STRING);
-if ($SendCadImg) {
-    $arquivo = $_FILES['inserirFoto'];
-    for ($cont = 0; $cont < count($arquivo['name']); $cont++) {
-        $nome = $_FILES['inserirFoto']['name'];
-        echo $nome[$cont];
-        // Pega a extensao
-    $extensao= strrchr($nome[$cont], '.');
-    // Converte a extensao para mimusculo
-    $extensao = strtolower($extensao);
-    echo"Extensao aqui" .$extensao;
-        if(strstr('.jpg;.jpeg;.gif;.png', $extensao))
-            {
-                
-             
-                $resultado_final = "img_".md5($nome[$cont].time());
-           
-                $destino = "imagens/" . $resultado_final.$extensao;
-                if (move_uploaded_file($arquivo['tmp_name'][$cont], $destino)) {
-                    echo "Upload realizado com sucesso";
-                } else {
-                        echo "Erro ao realizar upload";
-                    }
+            $arquivo = $_FILES['inserirFoto'];
+   
+            if(empty($arquivo['name'][0])){
+                //METODO PARA A POSTAGEM SEM FOTO
+                $sql = "INSERT INTO `HISTORICO_DESAFIO` (`PESSOA_ID`, `DESAFIO_ID`, `GRUPO_ID`, `POSTAGEM`) VALUES ('$perfil_usu','1', '$grupo', '$postagem')";
+                $db = $this->conn->prepare($sql);
+                $db->execute();
             }
-        else
-         {
-            echo "Você poderá enviar apenas arquivos \"*.jpg;*.jpeg;*.gif;*.png\"<br />";
-        }
-
-      
-    }
-} else {
-            echo "Erro ao realizar upload";
-    
+            else{
+                    if ($SendCadImg) {
+                        //METODO PARA A POSTAGEM COM FOTO
+                        for ($cont = 0; $cont < count($arquivo['name']); $cont++) 
+                            {
+                                $nome = $_FILES['inserirFoto']['name'];
+                                // Pega a extensao
+                                $extensao= strrchr($nome[$cont], '.');
+                                // Converte a extensao para mimusculo
+                                $extensao = strtolower($extensao);
+                                if(strstr('.jpg;.jpeg;.gif;.png', $extensao))
+                                    {
+                                    $resultado_final = "img_".md5($nome[$cont].time());
+                                    $destino = "../imagens_post/" . $resultado_final.$extensao;
+                                    if (move_uploaded_file($arquivo['tmp_name'][$cont], $destino)) {
+                                        $sql = "INSERT INTO `HISTORICO_DESAFIO` (`PESSOA_ID`, `DESAFIO_ID`, `GRUPO_ID`, `POSTAGEM`) VALUES ('$perfil_usu','1', '$grupo', '$postagem')";
+                                        $db = $this->conn->prepare($sql);
+                                        $db->execute();
+                                        $sql2 ="SELECT MAX(HISTORICO_ID) AS ID FROM HISTORICO_DESAFIO WHERE PESSOA_ID = ".$perfil_usu;
+                                        $db2 = $this->conn->prepare($sql2);
+                                        $db2->execute();
+                                        $linha = $db2->fetchAll();
+                                        foreach ($linha as $value){
+                                            $ID_PARA_ENVIO = $value['ID'];
+                                        }
+                                        $sql3 = "INSERT INTO `POST_MIDIA` (`MIDIA_TIPO_ARQUIVO`, `MIDIA_URL_TEXT`, `HISTORICO_ID`) VALUES ('IMAGEM','$destino', '$ID_PARA_ENVIO')";
+                                        $db3 = $this->conn->prepare($sql3);
+                                        $db3->execute();
+                                        header('Location: http://localhost/portal/index.php?page=grupo');
+                                    } else {
+                                            echo "Erro ao realizar upload";
+                                        }
+                                }else{  
+                                    echo "Você poderá enviar apenas arquivos \"*.jpg;*.jpeg;*.gif;*.png\"<br />";
+                                    }
+                                }
+                    } else {
+                            echo "Erro ao realizar upload";
+                    }
+                }
+            }
 }
+
+$postagem = new postagem();
+        
