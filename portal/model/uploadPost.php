0fa28215188875dc6aa5b0186c2143fc29aacea3
ggpgutierrez <34143293+ggpgutierrez@users.noreply.github.com>
2018-09-07 16:48:50 -0300

    Método de Upload finalizado

--- portal/model/uploadPost.php
+++ portal/model/uploadPost.php
@@ -11,12 +11,30 @@
             $this->conn = $conexao->conect();
             $grupo = $_SESSION['idUser_portal'];
             $postagem = $_POST['postDesafio'];
+            $linkVideo = $_POST['videoLink'];
+            echo ($linkVideo);
+
             $perfil_usu = $_SESSION['ID_PERFIL'];
             $SendCadImg = filter_input(INPUT_POST, 'SendCadImg', FILTER_SANITIZE_STRING);
 
             $arquivo = $_FILES['inserirFoto'];
-   
-            if(empty($arquivo['name'][0])){
+            if($linkVideo){
+                $sql = "INSERT INTO `HISTORICO_DESAFIO` (`PESSOA_ID`, `DESAFIO_ID`, `GRUPO_ID`, `POSTAGEM`) VALUES ('$perfil_usu','1', '$grupo', '$postagem')";
+                $db = $this->conn->prepare($sql);
+                $db->execute();
+                $sql2 ="SELECT MAX(HISTORICO_ID) AS ID FROM HISTORICO_DESAFIO WHERE PESSOA_ID = ".$perfil_usu;
+                $db2 = $this->conn->prepare($sql2);
+                $db2->execute();
+                $linha = $db2->fetchAll();
+                foreach ($linha as $value){
+                    $ID_PARA_ENVIO = $value['ID'];
+                }
+                $sql3 = "INSERT INTO `POST_MIDIA` (`MIDIA_TIPO_ARQUIVO`, `MIDIA_URL_TEXT`, `HISTORICO_ID`) VALUES ('VIDEO','$linkVideo', '$ID_PARA_ENVIO')";
+                $db3 = $this->conn->prepare($sql3);
+                $db3->execute();
+                header('Location: ../index.php?page=grupo');
+            }
+            if(empty($arquivo['name'][0]) && empty($linkVideo) ){
                 //METODO PARA A POSTAGEM SEM FOTO
                 $sql = "INSERT INTO `HISTORICO_DESAFIO` (`PESSOA_ID`, `DESAFIO_ID`, `GRUPO_ID`, `POSTAGEM`) VALUES ('$perfil_usu','1', '$grupo', '$postagem')";
                 $db = $this->conn->prepare($sql);
@@ -50,7 +68,7 @@
                                         $sql3 = "INSERT INTO `POST_MIDIA` (`MIDIA_TIPO_ARQUIVO`, `MIDIA_URL_TEXT`, `HISTORICO_ID`) VALUES ('IMAGEM','$destino', '$ID_PARA_ENVIO')";
                                         $db3 = $this->conn->prepare($sql3);
                                         $db3->execute();
-                                        header('Location: http://localhost/portal/index.php?page=grupo');
+                                        header('Location: ../index.php?page=grupo');
                                     } else {
                                             echo "Erro ao realizar upload";
                                         }
